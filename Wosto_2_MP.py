29443f230e7c63e1db2715e19d34980909a974d1
Fang-Omega <fangjingshuo321@163.com>
2024-02-17 16:38:50 +0800

    Brekono-05(2m17d5b)

--- Wosto_2_MP.py
+++ Wosto_2_MP.py
@@ -5,6 +5,7 @@ Created on Thu Feb 10 21:14:32 2022
 """
 from decimal import *
 from os import *
+from shutil import *
 
 supmap = str.maketrans('1234567890', '¹²³⁴⁵⁶⁷⁸⁹⁰')
 def sure(do='pass'):
@@ -110,5 +111,67 @@ while True:
             print(f'\33[31mError: Can\'t find "{rmdata}"')
         except:
             print('\33[31mError: Unable to recognize the inputs\33[0m')
-
-
+        else:
+            print('Successfully executed.')
+    elif 'rmdir' in inputs:
+        try:
+            deldir = inputs[1]
+            sure('rmtree(deldir)')
+        except FileNotFoundError:
+            print(f'\33[31mError: Can\'t find "{deldir}"')
+        except:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+        else:
+            print('Successfully executed.')
+    elif 'ls' in inputs or 'dir' in inputs:
+        try:
+            if '-a' in inputs:
+                hasp = 2
+            else:
+                hasp = 1
+            ls = listdir(inputs[hasp])
+        except:
+            ls = listdir(getcwd())
+        for list in range(len(ls)):
+            if ls[list][0] == '.' and hasp == 1:
+                continue
+            print("\33[34m" + ls[list] + "\33[0m")
+    elif 'pwd' in inputs:
+        print("\33[34m" + getcwd() + "\33[0m")
+    elif 'cp' == inputs[0]:
+        # 提醒：复制到的地点需要是目录或是文件，如是文件，被复制到的文件将会被替换为原文件。
+        # 如果想复制到指定目录中，可以使用/分隔目录，比如path/one/dir,path/one/file.txt等。
+        try:
+            cpdata = inputs[1]
+            path = inputs[2]
+            copy2(cpdata, path)
+        except FileNotFoundError:
+            print(f'\33[31mError: Can\'t find "{cpdata}"')
+        except:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+        else:
+            print('Successfully executed.')
+    elif 'move' == inputs[0]:
+        # 提醒：移动到的地点需要是目录，如目标位置不存在，原文件将会被重命名为目标位置。
+        # 如果您想复制到指定目录中，可以使用/分隔目录，比如path/one/dir,path/one/file.txt等。
+        try:
+            movedata = inputs[1]
+            path = inputs[2]
+            move(movedata, path)
+        except FileNotFoundError:
+            print(f'\33[31mError: Can\'t find "{movedata}"')
+        except:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+        else:
+            print('Successfully executed.')
+    elif 'rename' == inputs[0]:
+        try:
+            renamedata = inputs[1]
+            newname = inputs[2]
+            rename(renamedata, newname)
+        except FileNotFoundError:
+            print(f'\33[31mError: Can\'t find "{renamedata}"')
+        except:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+        else:
+            print('Successfully executed.')
