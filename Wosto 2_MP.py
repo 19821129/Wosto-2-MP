0979e009e882aba64556a7dee9e1222824985e5f
Fang-Omega <fangjingshuo321@163.com>
2024-02-17 15:11:46 +0800

    Brekono-03(2m17d3b)

--- Wosto 2_MP.py
+++ Wosto 2_MP.py
@@ -5,6 +5,17 @@ Created on Thu Feb 10 21:14:32 2022
 """
 from decimal import *
 
+supmap = str.maketrans('1234567890', '¹²³⁴⁵⁶⁷⁸⁹⁰')
+def sure():
+    while True:
+        bool = input('sure?(y/n)')
+        if bool == 'y':
+            return True
+        elif bool == 'n':
+            return False
+        else:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+
 while True:
     inputstr = input("|- Wosto 2 MP -| >>> ")
     inputstr = inputstr.lower()
@@ -69,7 +80,6 @@ while True:
                     print('\33[31mError: Unable to recognize the number entered\33[0m')
             if 'power' or 'square' in exprinput:
                 try:
-                    supmap = str.maketrans('1234567890', '¹²³⁴⁵⁶⁷⁸⁹⁰')
                     if 'power' in exprinput:
                         poweresult = Decimal(exprinput[1]) ** Decimal(exprinput[2])
                         print(exprinput[1] + exprinput[2].translate(supmap), '=', poweresult)
@@ -78,7 +88,17 @@ while True:
                         print(exprinput[1] + '²', '=', poweresult)
                 except:
                     print('\33[31mError: Unable to recognize the number entered\33[0m')
-
             if 'back' in exprinput or 'escape' in exprinput:
                 break
+    elif 'type' in inputs:
+        try:
+            readfile = inputs[1]
+            with open(readfile, 'r') as fileread:
+                readata = fileread.read()
+                print(readata)
+        except FileNotFoundError:
+            print(f'\33[31mError: Can\'t find "{readfile}"\33[0m')
+        except:
+            print('\33[31mError: Unable to recognize the inputs\33[0m')
+
 
